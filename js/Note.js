9980140473736187fac3b90bc3562fc72a73ea83
DrMayx <mj6367@gmail.com>
2018-08-21 20:50:24 +0200

    Add Removing notes; add changing title and description of notes

--- js/Note.js
+++ js/Note.js
@@ -1,10 +1,97 @@
+var numberOfNotes = 0;
 function Note(){
 
     this.x = 10;
     this.y = 10;
+    this.width = 100;
+    this.height = 100;
+    this.title = "Title";
+    this.desc = "Description ...\nbla bla bla";
+    this.id;
 
     this.show = function(){
-        fill(0);
-        rect(this.x, this.y, 100, 100);
+        var note = document.createElement("div");
+        note.id = "note" + numberOfNotes++;
+        this.id = note.id;
+        note.className = "note";
+        note.addEventListener("dblclick", this.editData);
+        note.addEventListener("contextmenu", this.changeData);
+        var content = document.createElement("table");
+        var titlerow = document.createElement("tr");
+        var titlecol = document.createElement("td");
+        var title = document.createElement("p");
+        title.innerText = this.title;
+        title.className = "title";
+        var descrow = document.createElement("tr");
+        var desccol = document.createElement("td");
+        var descr = document.createElement("p");
+        descr.innerText = this.desc;
+        descr.className = "description";
+        descr.addEventListener("dblclick", this.editDescr);
+        desccol.appendChild(descr);
+        descrow.appendChild(desccol);
+        titlecol.appendChild(title);
+        titlerow.appendChild(titlecol);
+        content.appendChild(titlerow);
+        content.appendChild(descrow);
+        note.appendChild(content);
+
+        document.getElementById("panel").appendChild(note);
+    };
+
+    this.registerEventListeners = function(){
+        //this.
+        console.log("09090")
+    };
+
+    this.editData = function(){
+        if(noteEdited){
+            return;
+        }
+        noteEdited = true;
+        var title = this.getElementsByClassName("title")[0];
+        var desc = this.getElementsByClassName("description")[0];
+
+        title.innerHTML = "<input type='text' id='titleEdit' name='title' placeholder='Title'>";
+        desc.innerHTML = "<input type='text' id='descEdit' name='description' placeholder='Description'>";
+    };
+
+    this.changeData = function (event){
+        if(event != null){
+            event.preventDefault();
+        }
+        let note = getCurrentNote(event);
+        if(!noteEdited){
+            if(note) {
+                if (confirm("Delete " + note.title + " ?")) {
+                    var panel = document.getElementById("panel");
+                    panel.removeChild(note);
+                }
+            }
+            return;
+        }
+
+        var newTitle = document.getElementById("titleEdit").value;
+        var newDescr = document.getElementById("descEdit").value;
+        if(newTitle !== ""){
+            note.title = newTitle;
+        }
+        if(newDescr !== ""){
+            note.desc = newDescr;
+        }
+        var title = this.getElementsByClassName("title")[0];
+        var desc = this.getElementsByClassName("description")[0];
+        title.innerHTML = "<p class='title'>" + note.title + "</p>";
+        desc.innerHTML = "<p class='description'>" + note.desc + "</p>";
+        noteEdited = false;
+    }
+
+    var getCurrentNote = function (event){
+        var path = event.path;
+        for(var i=0; i<path.length; i++){
+            if(path[i].id.includes("note")){
+                return path[i];
+            }
+        }
     }
 }
--- js/notes.js
+++ js/notes.js
@@ -1,20 +1,24 @@
 "use strict";
 
 var notes = [];
-var setup = function() {
-    var paragraph = document.createElement("h3");
+var noteEdited = false;
+window.onload = function (){
+    var paragraph = document.createElement("h2");
     paragraph.innerText = "Not-A-Pad by DrMayx.";
     var panel = document.getElementById("panel");
+    var usage = document.createElement("p");
+    usage.innerHTML = "<b>DoubleClick on notes title or description to change it.<br>Rightclick on it to save.";
     panel.appendChild(paragraph);
+    panel.appendChild(usage);
     addNoteButton();
-    createCanvas(window.innerWidth/2, window.innerHeight/2);
+    //window.addEventListener("keypress", this.handleKeyPress);
 };
 
 var draw = function(){
-    background(100,200,237);
-    for(var i = 0; i<notes.length; i++){
-        notes[i].show();
-    }
+    //background(100,200,237);
+    // for(var i = 0; i<notes.length; i++){
+    //     notes[i].show();
+    // }
 };
 var addNoteButton = function() {
     var button = document.createElement("input");
@@ -28,5 +32,24 @@ var addNoteButton = function() {
 var createNewNote = function(){
     console.log("creating new note...");
     var n = new Note();
+    n.show();
     notes.push(n);
 };
+
+var getNote = function(noteId){
+    for(var i = 0; i < notes.length; i++){
+        if(notes[i].id===noteId){
+            return notes[i];
+        }
+    }
+    return undefined;
+};
+
+var getEditedNoteId = function(){
+    return getEditedNote().id;
+}
+
+var getEditedNote = function (){
+    return document.getElementById("titleEdit").parentElement.parentElement.parentElement.parentElement.parentElement;
+}
+
